bb98e0a394758871571a93e60e43c29c7742320d
Pham Van Nguyen <nguyenpv@fint.vn>
2024-04-08 16:06:11 +0700

    done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,7 @@ function Home() {
         <h1 className="text-6xl font-black text-center text-slate-900 mb-6">
         PHẦN MỀM BÁO CÁO CHI HỘI
         </h1>
-        <p className="text-xl text-center mb-6">
-          Join other developers who are learning how to build websites and
-          applications with React.js.
-        </p>
+        
         <p className="text-center">
           <Button href="/contact">
             BẮT ĐẦU BÁO CÁO
